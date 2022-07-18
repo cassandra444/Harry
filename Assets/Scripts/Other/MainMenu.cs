799e0ba2de561d6566d61199d6b7cdc715653848
cassandra444 <60215433+cassandra444@users.noreply.github.com>
2022-07-18 22:51:27 +0200

    Main Menu Script
    - play button : load game scene | quit button : quit game
    - change sprite button on overlap
    - make noise on averlap

---
+++ Assets/Scripts/Other/MainMenu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Play Button")]
+    [SerializeField] private AudioSource _OnButtonSound;
+
+    [Header("Play Button")]
+    public Image playImage;
+    public Sprite unselectPlayImage;
+    public Sprite selectPlayImage;
+
+    [Header("Play BUtton")]
+    public Image quitImage;
+    public Sprite unselectQuitImage;
+    public Sprite selectQuitImage;
+
+
+    #region Play and Quit
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StopGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+
+    #region Change Sprites
+    public void ChangePlay()
+    {
+        playImage.sprite = selectPlayImage;
+        _OnButtonSound.Play();
+    }
+
+    public void BackPlay()
+    {
+        playImage.sprite = unselectPlayImage;
+    }
+
+    public void ChangeQuit()
+    {
+        quitImage.sprite = selectQuitImage;
+        _OnButtonSound.Play();
+    }
+
+    public void BackQuit()
+    {
+        quitImage.sprite = unselectQuitImage;
+    }
+
+  
+    #endregion
+
+
+}
