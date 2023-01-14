8dd390df46142fa3032b14be716ae66f8b9a4653
cassandra444 <60215433+cassandra444@users.noreply.github.com>
2023-01-14 10:10:10 +0100

    Test Mouvement Caméra

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private Transform Pos1;
+    [SerializeField] private Transform Pos2;
+    //[SerializeField] private Transform Pos3;
 
     private void Update()
     {
         transform.LookAt(target);
         //transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+        //if (Input.GetKey(KeyCode.Keypad1)) transform.position = Pos1.transform.position;
+        if (Input.GetKey(KeyCode.Keypad2)) transform.position = Pos2.transform.position;
+        //if (Input.GetKey(KeyCode.Keypad3)) transform.position = Pos3.transform.position;
+ 
     }
 }
